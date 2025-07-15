0b42f407fbdda8b4c6783614f0a88bbc0607992c
mudit0829 <mekasutechnology@gmail.com>
2025-07-15 14:44:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,3 +13,6 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
+app.get("/", (req, res) => {
+  res.send("Backend is working");
+});
