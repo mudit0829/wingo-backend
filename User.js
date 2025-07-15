b65bf7f6f3a3664159d23f933ca3f08bb4ea4142
mudit0829 <mekasutechnology@gmail.com>
2025-07-15 15:01:17 +0530

    Delete User.js

--- User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: String // 'admin' or 'user'
-});
-
-module.exports = mongoose.model("User", userSchema);
