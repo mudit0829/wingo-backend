e5b4468224103a5c4485b8055327036323f4a484
mudit0829 <mekasutechnology@gmail.com>
2025-07-15 11:09:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+  .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => console.log("Server running on port 5000"));
-})
-.catch(err => console.error(err));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.error(err));
