c448a319af96cbb6bc978b443619fabfa5ab888a
mudit0829 <mekasutechnology@gmail.com>
2025-07-17 00:01:39 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  role: String
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "user"], required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
