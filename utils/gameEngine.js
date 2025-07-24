d7c474e40a3b3de57bd80dfb72a7f08f1baf8c35
mudit0829 <mekasutechnology@gmail.com>
2025-07-24 16:23:34 +0530

    Update gameEngine.js

--- utils/gameEngine.js
+++ utils/gameEngine.js
@@ -1,6 +1,6 @@
-function getWinningColor(number) {
-  if (number === 5) return "VIOLET";
-  return [1,3,7,9].includes(number) ? "RED" : "GREEN";
+// utils/gameEngine.js
+function getWinningColor(num) {
+  if (num === 5) return 'VIOLET';
+  return [1,3,7,9].includes(num) ? 'RED' : 'GREEN';
 }
-
 module.exports = { getWinningColor };
