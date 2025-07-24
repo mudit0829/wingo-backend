b9f2757dada8b00412d2310629c1efd441a0f4c6
mudit0829 <mekasutechnology@gmail.com>
2025-07-24 16:21:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const betRoutes = require("./routes/betRoutes");
-const cronRoutes = require("./routes/cronRoutes");
-const resetRoute = require("./routes/resetRoute");
-const { startLoop } = require("./utils/roundManager");
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const betRoutes = require('./routes/betRoutes');
+const cronRoutes = require('./routes/cronRoutes');
+const roundRoutes = require('./routes/roundRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/wingo", { useNewUrlParser: true, useUnifiedTopology: true })
+app.use('/api/bets', betRoutes);
+app.use('/api/cron', cronRoutes);
+app.use('/api/rounds', roundRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => res.send('Wingo backend running'));
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("MongoDB connected");
-    startLoop(); // start the automatic round cycle
-  });
-
-app.use(betRoutes);
-app.use(cronRoutes);
-app.use(resetRoute);
-
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    );
+  })
+  .catch(err => console.error(err));
