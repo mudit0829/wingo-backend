a911dbb9a43bfe7bc711dbcd7f76c91e852f6231
mudit0829 <mekasutechnology@gmail.com>
2025-07-24 11:20:26 +0530

    Update Round.js

--- models/Round.js
+++ models/Round.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-const RoundSchema = new mongoose.Schema({
-  roundId: { type: Number, required: true, unique: true },
-  result: { type: String, enum: ["0","1","2","3","4","5","6","7","8","9"], default: null },
-  timestamp: { type: Date, default: Date.now },
+const roundSchema = new mongoose.Schema({
+  roundId: String,
+  result: Number,
+  winningColor: String,
+  startTime: Date,
+  endTime: Date,
+  bets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Bet" }],
+  profitLoss: Number
 });
 
-module.exports = mongoose.model("Round", RoundSchema);
+module.exports = mongoose.model("Round", roundSchema);
