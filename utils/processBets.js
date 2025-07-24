f00b353fbe0520d4d014a59077bc1a963962854d
mudit0829 <mekasutechnology@gmail.com>
2025-07-24 17:56:24 +0530

    Create processBets.js

---
+++ utils/processBets.js
@@ -0,0 +1,36 @@
+const Result = require('../models/Result');
+const Bet = require('../models/Bet');
+const User = require('../models/User');
+
+const processBets = async (roundId, result) => {
+  const bets = await Bet.find({ roundId });
+
+  for (const bet of bets) {
+    const user = await User.findById(bet.userId);
+    if (!user) continue;
+
+    let winnings = 0;
+
+    // Payout logic
+    if (bet.type === 'color') {
+      if (
+        (bet.value === 'RED' && [1, 3, 7, 9].includes(result)) ||
+        (bet.value === 'GREEN' && [2, 4, 6, 8].includes(result)) ||
+        (bet.value === 'VIOLET' && [0, 5].includes(result))
+      ) {
+        winnings = bet.amount * (bet.value === 'VIOLET' ? 4.5 : 2);
+      }
+    } else if (bet.type === 'number' && bet.value == result) {
+      winnings = bet.amount * 9;
+    }
+
+    if (winnings > 0) {
+      user.balance += winnings;
+      await user.save();
+    }
+  }
+
+  await Result.findByIdAndUpdate(roundId, { result });
+};
+
+module.exports = processBets;
