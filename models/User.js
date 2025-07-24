1a76d1da8bae509aaf11271df8a615b4b74080ad
mudit0829 <mekasutechnology@gmail.com>
2025-07-24 11:19:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: String,
+  password: String,
   balance: { type: Number, default: 0 },
   winHistory: [{ roundId: String, amountWon: Number }]
 });
