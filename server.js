31106252c732f1cdf999cd46145a6d64780a62af
mudit0829 <mekasutechnology@gmail.com>
2025-07-25 18:28:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,13 @@ const cronRoutes = require('./routes/cronRoutes');
 
 dotenv.config();
 connectDB();
-
 const app = express();
-
 app.use(cors());
-app.options('*', cors());
 app.use(express.json());
 
 app.get('/ping', (req, res) => res.send('pong'));
-app.get('/api/health', (req, res) => res.send('API is running 🚀'));
-
 app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api/cron', cronRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
