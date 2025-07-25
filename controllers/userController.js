4a51d8674ce86d044ae44bcb55afe93a0a35db22
mudit0829 <mekasutechnology@gmail.com>
2025-07-25 19:04:10 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,13 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 
-// @desc    Register new user
-// @route   POST /api/users/register
-// @access  Public
+// @desc    Register a new user
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
+  if (!email || !password || !name) {
     res.status(400);
-    throw new Error('Name, email, and password are required');
+    throw new Error('Please enter all fields');
   }
 
   const userExists = await User.findOne({ email });
@@ -20,11 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists');
   }
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await User.create({ name, email, password });
 
   if (user) {
     res.status(201).json({
@@ -40,8 +34,6 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // @desc    Auth user & get token
-// @route   POST /api/users/login
-// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
