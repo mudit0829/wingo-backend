d9ce7c61613e900c04392a6154204bf598b2b4b8
mudit0829 <mekasutechnology@gmail.com>
2025-07-25 18:05:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 const cronRoutes = require('./routes/cronRoutes');
-const dotenv = require('dotenv');
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 
 app.get('/ping', (req, res) => res.send('pong'));
 app.get('/api/health', (req, res) => res.send('API is running 🚀'));
+
 app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api/cron', cronRoutes);
 
-app.listen(process.env.PORT || 5000, () => console.log('Server started'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
