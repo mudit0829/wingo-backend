913d12173c2a3b09b2f21e7e851f5e4278e40068
mudit0829 <mekasutechnology@gmail.com>
2025-07-25 22:02:30 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, authUser } = require('../controllers/userController');
+const { registerUser, authUser } = require("../controllers/userController");
 
-router.post('/register', registerUser);
-router.post('/login', authUser);
+router.post("/register", registerUser);
+router.post("/login", authUser);
 
 module.exports = router;
