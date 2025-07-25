d100e8a7e6d5499dbda811ac7201b25b2eb0f278
mudit0829 <mekasutechnology@gmail.com>
2025-07-25 21:51:04 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'secretkey', {
+  return jwt.sign({ id }, process.env.JWT_SECRET || 'yourSecretKey', {
     expiresIn: '30d',
   });
 };
