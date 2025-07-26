8c0048baacca2e2bb298f8c3af07d08fc53bed78
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 20:48:55 +0530

    Update bet.js

--- models/bet.js
+++ models/bet.js
@@ -1,11 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const betSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  round: String, // round ID or number
-  type: String, // "color" or "number"
-  value: String, // "RED", "GREEN", "VIOLET" or a number like "5"
-  amount: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  round: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Round',
+    required: true,
+  },
+  color: {
+    type: String,
+    enum: ['RED', 'GREEN', 'VIOLET'],
+  },
+  number: {
+    type: Number,
+    min: 0,
+    max: 9,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
 }, { timestamps: true });
 
-module.exports = mongoose.model("Bet", betSchema);
+module.exports = mongoose.model('Bet', betSchema);
