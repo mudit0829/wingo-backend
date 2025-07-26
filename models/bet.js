23d129a674f6d57c8329c0c2ab16d86461b5bf8f
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 19:23:49 +0530

    Update bet.js

--- models/bet.js
+++ models/bet.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const betSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  round: String,
+  round: String, // round ID or number
   type: String, // "color" or "number"
-  value: String, // "RED", "GREEN", or "0"–"9"
+  value: String, // "RED", "GREEN", "VIOLET" or a number like "5"
   amount: Number,
 }, { timestamps: true });
 
