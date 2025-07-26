25570ffd75dfc9f2b0bb656fd993049cd620e245
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 23:20:31 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, authUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getWallet,
+} = require("../controllers/userController");
 
+const authenticate = require("../middleware/authenticate");
+
+// Auth routes
 router.post("/register", registerUser);
-router.post("/login", authUser);
+router.post("/login", loginUser);
+
+// Wallet route (requires JWT token)
+router.get("/wallet", authenticate, getWallet);
 
 module.exports = router;
