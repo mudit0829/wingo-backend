5acf8af46ef2e6f6473f4cec142b0ee800d9a92e
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 09:02:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,20 +9,32 @@ const betRoutes = require('./routes/betRoutes');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose
-  .connect('your_mongodb_connection_string_here', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    'mongodb+srv://mekasutechnology:Wingo123%21@cluster0.x1btj4f.mongodb.net/wingoDB?retryWrites=true&w=majority&appName=Cluster0',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
-app.use('/api', authRoutes);
-app.use('/api', betRoutes);
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/bet', betRoutes);
 
+// Root test route
+app.get('/', (req, res) => {
+  res.send('WinGo Backend is running...');
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
