961252bd9147ee30d754abbfca532dfca6f89f8a
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 08:51:04 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,25 +1,9 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    balance: { type: Number, default: 1000 },
-  },
-  { timestamps: true }
-);
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  balance: { type: Number, default: 1000 },
 });
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.model('user', userSchema);
