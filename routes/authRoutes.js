d86e3d7f2d34e8c7f75b0369373f33be34f8111b
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 16:55:48 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -28,7 +28,7 @@ router.post("/login", async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
-        const token = jwt.sign({ userId: user._id }, "secretkey", { expiresIn: "1h" });
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.status(200).json({ token });
     } catch (error) {
         res.status(500).json({ message: "Login failed" });
