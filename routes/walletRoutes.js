ef2195b85027377f0b0f22c82b52754801b90460
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 10:56:45 +0530

    Update walletRoutes.js

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../middleware/authenticate");
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.get("/", authenticate, async (req, res) => {
   try {
