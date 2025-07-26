dd4e3d79cee964de5ea899cad1d0f0fd61b35b27
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 09:58:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./routes/authRoutes");
+const betRoutes = require("./routes/betRoutes");
+const resultRoutes = require("./routes/resultRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api", authRoutes);
+app.use("/api/bets", betRoutes);
+app.use("/api/results", resultRoutes);
+
+mongoose.connect("mongodb+srv://mekasutechnology:Wingo123%21@cluster0.x1btj4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
