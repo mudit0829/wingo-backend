fe08291ae0a88cf49cc3a077d207d9354169636a
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 09:57:21 +0530

    Delete models/user.js

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  balance: { type: Number, default: 1000 },
-});
-
-module.exports = mongoose.model('user', userSchema);
