f9526da853dc8db19dea77fc82f8be5608ed2038
mudit0829 <mekasutechnology@gmail.com>
2025-07-26 08:43:16 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const user = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 router.post("/login", async (req, res) => {
