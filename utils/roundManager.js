fcc04ebc9c1a82cc9e877cf64eacf3186840e192
mudit0829 <mekasutechnology@gmail.com>
2025-07-28 19:45:41 +0530

    Update roundManager.js

--- utils/roundManager.js
+++ utils/roundManager.js
@@ -2,11 +2,23 @@ const axios = require("axios");
 
 function startLoop() {
   setInterval(async () => {
-    const r = await axios.post("http://localhost:3000/cron/start-timer");
-    setTimeout(() => {
-      axios.post("http://localhost:3000/cron/generate-result", { roundId: r.data.round.roundId });
-    }, 30000);
-  }, 30000);
+    try {
+      const r = await axios.post("http://localhost:3000/cron/start-timer");
+      console.log('[📦] Round started:', r.data);
+
+      setTimeout(() => {
+        axios.post("http://localhost:3000/cron/generate-result", {
+          roundId: r.data.round.roundId
+        }).then(res => {
+          console.log('[🎯] Result generated:', res.data);
+        }).catch(err => {
+          console.error('[❌] Error generating result:', err.message);
+        });
+      }, 25000); // wait 25 seconds for betting
+    } catch (err) {
+      console.error('[❌] Error starting round:', err.message);
+    }
+  }, 30000); // every 30s
 }
 
 module.exports = { startLoop };
