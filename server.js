c69a88cf1b1e6cf2304940c91d26bf2c5289d420
mudit0829 <mekasutechnology@gmail.com>
2025-07-30 09:49:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const userRoutes = require("./routes/userRoutes");
 
 // Load environment variables
 dotenv.config();
