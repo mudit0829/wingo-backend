603da0563946f79455f90e4afbcfb54c31c7c180
mudit0829 <mekasutechnology@gmail.com>
2025-07-30 10:31:56 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Wallet balance fetch route
 router.get('/wallet/:email', async (req, res) => {
