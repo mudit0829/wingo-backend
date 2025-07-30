2ca9c8ccb2d71e3fe3a1630828695ca72879720a
mudit0829 <mekasutechnology@gmail.com>
2025-07-30 17:03:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const betRoutes = require('./routes/betRoutes');
-const resetRoute = require('./routes/resetRoute');
 
 dotenv.config();
 const app = express();
@@ -12,17 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB error:', err));
+// Route imports
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const betRoutes = require('./routes/betRoutes');
+const roundRoutes = require('./routes/roundRoutes');
 
+// Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/bets', betRoutes);
-app.use('/api/users', resetRoute);
+app.use('/api/rounds', roundRoutes);
 
-app.get('/', (req, res) => {
-  res.send('WinGo backend running...');
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
