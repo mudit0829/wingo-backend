4a1d2ee2febb6e9f32c423f4c8497720af9c1ea8
mudit0829 <mekasutechnology@gmail.com>
2025-08-01 13:29:55 +0530

    Update processBets.js

--- utils/processBets.js
+++ utils/processBets.js
@@ -1,46 +1,43 @@
+// utils/processBets.js
+
 const Bet = require('../models/bet');
 const User = require('../models/user');
 
 const processBets = async (roundId, result) => {
   const bets = await Bet.find({ roundId });
+  const winningColor = result.color;
+  const winningNumber = result.number;
 
   for (const bet of bets) {
-    const user = await User.findById(bet.userId);
-    if (!user) continue;
+    const effectiveAmount = bet.amount * 0.98;
+    let payout = 0;
 
-    const serviceFee = 0.02;
-    const effectiveAmount = bet.amount * (1 - serviceFee);
-    let winnings = 0;
-
-    const num = result;
-
-    if (bet.type === 'color') {
-      const color = bet.value;
-
-      if ((color === 'GREEN' && [1, 3, 7, 9].includes(num)) ||
-          (color === 'RED' && [2, 4, 6, 8].includes(num))) {
-        winnings = effectiveAmount * 2;
-      } else if (color === 'VIOLET' && [0, 5].includes(num)) {
-        winnings = effectiveAmount * 4.5;
-      } else if ((color === 'GREEN' && num === 5) || (color === 'RED' && num === 0)) {
-        winnings = effectiveAmount * 1.5;
+    // Color bet evaluation
+    if (bet.color && bet.color === winningColor) {
+      if ((winningColor === 'Green' && winningNumber === 5) || (winningColor === 'Red' && winningNumber === 0)) {
+        payout += effectiveAmount * 1.5;
+      } else {
+        payout += effectiveAmount * 2;
       }
-
-    } else if (bet.type === 'number' && parseInt(bet.value) === num) {
-      winnings = effectiveAmount * 9;
     }
 
-    if (winnings > 0) {
-      user.balance += winnings;
-      bet.status = 'win';
-      bet.winningAmount = winnings;
-    } else {
-      bet.status = 'lose';
-      bet.winningAmount = 0;
+    // Violet payout special case
+    if (bet.color === 'Violet' && (winningNumber === 0 || winningNumber === 5)) {
+      payout += effectiveAmount * 4.5;
     }
 
-    await user.save();
-    await bet.save();
+    // Number bet evaluation
+    if (typeof bet.number === 'number' && bet.number === winningNumber) {
+      payout += effectiveAmount * 9;
+    }
+
+    if (payout > 0) {
+      const user = await User.findOne({ email: bet.email });
+      if (user) {
+        user.wallet += payout;
+        await user.save();
+      }
+    }
   }
 };
 
