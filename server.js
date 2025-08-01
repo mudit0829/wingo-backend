62ede7e245bed12c553202a963c8c9a5c31b82e9
mudit0829 <mekasutechnology@gmail.com>
2025-08-01 21:57:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,3 +50,6 @@ mongoose.connect(process.env.MONGO_URI, {
 }).catch((error) => {
   console.error('Error connecting to MongoDB:', error.message);
 });
+// Start the game loop (round manager) after server starts
+const gameLoop = require('./gameLoop');
+gameLoop(); // start the loop
