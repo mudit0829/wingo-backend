fe4f9a0f09f49dcf534f992619f5a0bb3a812eba
mudit0829 <mekasutechnology@gmail.com>
2025-08-01 13:30:40 +0530

    Update gameLoop.js

--- utils/gameLoop.js
+++ utils/gameLoop.js
@@ -1,27 +1,29 @@
-const Round = require('../models/round');
+// gameLoop.js
+
+const Round = require('./models/round');
 const generateResult = require('./utils/generateResult');
+const processBets = require('./utils/processBets');
 
-let currentRound = null;
-let roundCounter = 1;
-const ROUND_DURATION = 30 * 1000; // 30 seconds
-const BETTING_TIME = 25 * 1000;   // 25 seconds
-
-const startNewRound = async () => {
-  const timestamp = new Date();
-  const roundId = `R${Date.now()}`;
-
-  currentRound = new Round({ roundId, timestamp });
-  await currentRound.save();
-  roundCounter++;
-};
-
-const gameLoop = async () => {
-  await startNewRound();
+async function runGameRound() {
+  const round = new Round({
+    roundId: Date.now().toString(),
+    timestamp: new Date()
+  });
+  await round.save();
 
   setTimeout(async () => {
-    await generateResult();
-    setTimeout(gameLoop, 5000); // Wait 5s after result, then start next round
-  }, BETTING_TIME);
-};
+    const result = generateResult();
+    round.result = result;
+    await round.save();
 
-module.exports = gameLoop;
+    await processBets(round.roundId, result);
+    console.log(`Processed round ${round.roundId} with result`, result);
+  }, 25000); // Draw result after 25 seconds
+}
+
+function startGameLoop() {
+  setInterval(runGameRound, 30000); // Run every 30 seconds
+  runGameRound(); // Initial run immediately
+}
+
+module.exports = startGameLoop;
