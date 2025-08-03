3a99d3c03c986780d3f4c53505c9600f78c2cae3
mudit0829 <mekasutechnology@gmail.com>
2025-08-03 17:31:34 +0530

    Update generateResult.js

--- utils/generateResult.js
+++ utils/generateResult.js
@@ -5,8 +5,8 @@ function getRandomInt(max) {
 
 function calculateColor(resultNumber) {
   if (resultNumber === 0 || resultNumber === 5) return 'Violet';
-  if ([1, 3, 7, 9].includes(resultNumber)) return 'Green';
-  if ([2, 4, 6, 8].includes(resultNumber)) return 'Red';
+  if ([1, 3, 7, 9].includes(resultNumber)) return 'Red';
+  if ([2, 4, 6, 8].includes(resultNumber)) return 'Green';
   return null;
 }
 
