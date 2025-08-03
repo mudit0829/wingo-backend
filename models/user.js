f621f92863cfab564b09fbaf48d90e4c842f597d
mudit0829 <mekasutechnology@gmail.com>
2025-08-03 17:41:28 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  balance: { type: Number, default: 1000 }
+  wallet: { type: Number, default: 1000 }  // ✅ Changed from 'balance' to 'wallet'
 });
 
 module.exports = mongoose.model('User', userSchema);
