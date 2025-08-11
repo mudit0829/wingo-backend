73128a2f2ea86dd2ebfb60175c5906cee0a91586
mudit0829 <mekasutechnology@gmail.com>
2025-08-11 19:16:03 +0530

    Update processBets.js

--- utils/processBets.js
+++ utils/processBets.js
@@ -1,9 +1,13 @@
 const Bet = require('../models/bet');
 const User = require('../models/user');
 
-async function processBets(roundId, result) {
-  const bets = await Bet.find({ roundId });
-  const winningNumber = result.number; // actual 0-9 result
+/**
+ * Process all bets for a given round string ID like "R-20250811-133530"
+ * and the result object: { number: 7, color: 'Red' }
+ */
+async function processBets(roundIdString, result) {
+  const bets = await Bet.find({ roundId: roundIdString });
+  const winningNumber = result.number;
 
   for (const bet of bets) {
     const amount = bet.amount;
@@ -12,7 +16,7 @@ async function processBets(roundId, result) {
     let payout = 0;
     let win = false;
 
-    // ----- COLOR BET LOGIC -----
+    // COLOR BET LOGIC
     if (bet.colorBet) {
       if (bet.colorBet === 'Red' && [2, 4, 6, 8, 0].includes(winningNumber)) {
         payout = contractAmount * 2;
@@ -28,18 +32,18 @@ async function processBets(roundId, result) {
       }
     }
 
-    // ----- NUMBER BET LOGIC -----
+    // NUMBER BET LOGIC
     if (typeof bet.numberBet === 'number' && bet.numberBet === winningNumber) {
       payout = contractAmount * 9;
       win = true;
     }
 
-    // Update bet in DB
+    // Update bet status & net amount
     bet.win = win;
     bet.netAmount = win ? payout : -amount;
     await bet.save();
 
-    // Credit winnings if win
+    // Credit wallet if win
     if (win && payout > 0) {
       const user = await User.findOne({ email: bet.email });
       if (user) {
