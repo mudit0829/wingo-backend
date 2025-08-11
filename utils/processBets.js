240fcb1be769f012951e5b2ebfbf7d00967f64ae
mudit0829 <mekasutechnology@gmail.com>
2025-08-11 19:02:17 +0530

    Update processBets.js

--- utils/processBets.js
+++ utils/processBets.js
@@ -2,17 +2,17 @@ const Bet = require('../models/bet');
 const User = require('../models/user');
 
 async function processBets(roundId, result) {
-  const bets = await Bet.find({ roundId }).populate('user');
-  const winningColor = result.color;    // 'Red', 'Green', 'Violet'
-  const winningNumber = result.number;  // 0-9
+  const bets = await Bet.find({ roundId });
+  const winningNumber = result.number; // actual 0-9 result
 
   for (const bet of bets) {
     const amount = bet.amount;
     const contractAmount = bet.contractAmount || (amount - 2);
+
     let payout = 0;
     let win = false;
 
-    // Color bets
+    // ----- COLOR BET LOGIC -----
     if (bet.colorBet) {
       if (bet.colorBet === 'Red' && [2, 4, 6, 8, 0].includes(winningNumber)) {
         payout = contractAmount * 2;
@@ -22,27 +22,30 @@ async function processBets(roundId, result) {
         payout = contractAmount * 2;
         win = true;
       }
-      if (bet.colorBet === 'Violet' && [0, 5].includes(winningNumber)) {
+      if (bet.colorBet === 'Violet' && (winningNumber === 0 || winningNumber === 5)) {
         payout = contractAmount * 4.5;
         win = true;
       }
     }
 
-    // Number bets
+    // ----- NUMBER BET LOGIC -----
     if (typeof bet.numberBet === 'number' && bet.numberBet === winningNumber) {
       payout = contractAmount * 9;
       win = true;
     }
 
-    // Update bet outcome
+    // Update bet in DB
     bet.win = win;
     bet.netAmount = win ? payout : -amount;
     await bet.save();
 
-    // Credit user if win
+    // Credit winnings if win
     if (win && payout > 0) {
-      bet.user.wallet += payout;
-      await bet.user.save();
+      const user = await User.findOne({ email: bet.email });
+      if (user) {
+        user.wallet += payout;
+        await user.save();
+      }
     }
   }
 }
