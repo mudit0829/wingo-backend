bd097ecd644921655f632e0a419cfd8dde0dba60
mudit0829 <mekasutechnology@gmail.com>
2025-09-25 13:04:44 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,9 @@ const userSchema = mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+
+    // New field for shopping points balance
+    shoppingPoints: { type: Number, default: 0 }
   },
   { timestamps: true }
 );
