613512c63fab895679a6e2f36f1c7a4672c16f80
mudit0829 <mekasutechnology@gmail.com>
2025-09-28 11:59:00 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   wallet: { type: Number, default: 1000 },
   agentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-  salaryEarned: { type: Number, default: 0 }
+  salaryEarned: { type: Number, default: 0 },
+  role: { type: String, default: 'user' } // add role with default
 });
 
 module.exports = mongoose.model('User', userSchema);
